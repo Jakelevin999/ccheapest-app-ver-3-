6585f6b6fc4ab059b4236fdaaf8f6bd68cff1564
Jakelevin999 <151411153+Jakelevin999@users.noreply.github.com>
2026-05-13 00:49:52 -0700

    Add controlled signup fields

--- components/onboarding/SignupStep.tsx
+++ components/onboarding/SignupStep.tsx
@@ -1,10 +1,30 @@
-export default function SignupStep() {
+interface Props {
+  name: string
+  setName: (value:string) => void
+  email: string
+  setEmail: (value:string) => void
+  phone: string
+  setPhone: (value:string) => void
+  password: string
+  setPassword: (value:string) => void
+}
+
+export default function SignupStep({
+  name,
+  setName,
+  email,
+  setEmail,
+  phone,
+  setPhone,
+  password,
+  setPassword
+}: Props) {
   return (
     <div style={{display:'grid',gap:14}}>
-      <input className='input' placeholder='Full name' />
-      <input className='input' placeholder='Email address' />
-      <input className='input' placeholder='Phone number' />
-      <input className='input' type='password' placeholder='Password' />
+      <input className='input' placeholder='Full name' value={name} onChange={(e)=>setName(e.target.value)} />
+      <input className='input' placeholder='Email address' value={email} onChange={(e)=>setEmail(e.target.value)} />
+      <input className='input' placeholder='Phone number' value={phone} onChange={(e)=>setPhone(e.target.value)} />
+      <input className='input' type='password' placeholder='Password' value={password} onChange={(e)=>setPassword(e.target.value)} />
     </div>
   )
 }
