1b1b2947efe7f5420ba2976e7a0e8c12867823d0
Jakelevin999 <151411153+Jakelevin999@users.noreply.github.com>
2026-05-12 22:25:46 -0700

    Create settings layout

---
+++ components/settings/SettingsLayout.tsx
@@ -0,0 +1,9 @@
+import AccountCenter from './AccountCenter'
+
+export default function SettingsLayout() {
+  return (
+    <div style={{display:'grid',gap:20,paddingBottom:120}}>
+      <AccountCenter />
+    </div>
+  )
+}
